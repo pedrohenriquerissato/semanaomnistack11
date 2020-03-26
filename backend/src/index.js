0583fd30ef2de3216f2b9b0486446dc0a23d8dcd
Pedro Henrique Rissato <pedrorissato@usp.br>
2020-03-25 23:44:53 -0300

    Inicilizando o repositorio

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+
+/**
+ * Allows body requests as JSON
+ */
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333); 
